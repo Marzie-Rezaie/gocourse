da5b078b69eb830aa9943df1d69feb05667e961a
Marzie <190611516+Marzie-Rezaie@users.noreply.github.com>
2025-09-04 11:18:55 +0500

    end of basics section

---
+++ basics/init.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func init() {
+	fmt.Println("Initializing something....")
+}
+func init() {
+	fmt.Println("Initializing something1....")
+}
+func init() {
+	fmt.Println("Initializing something2....")
+}
+
+func main() {
+	fmt.Println("Inside the main function")
+}
